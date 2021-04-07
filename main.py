826d9e74addaa7ac8a3f0bc56fa2841531f0c5d3
Julius Taylor <juliustaylor@protonmail.com>
2021-04-07 19:31:47 +0200

    Add command line args
    Because:
    - The experiment was not configurable from the command line
    
    This commit allows to define the configuration in the NamedTuple objects
    and automatically adds all fields from it to an argparser. They can be then overwritten
    from the command line by simply calling --arg value. Care must be taken with boolean flags,
    which currently use the eval() function as type conversion. This MIGHT lead to unexpected
    behaviour or produce non-clear error messages.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from experiments.shared_ref_mnist import Experiment
 from functions import run
 from typing import NamedTuple
+import argparse
 
 
 # TODO: implement command line overrides and use type hints to provide autocomplete
@@ -26,10 +27,10 @@ class Config(NamedTuple):
     sigma: float = 0.0
 
     # hyperparameters
-    eta_ae: float = 1
-    eta_lsa: float = 0
-    eta_msa: float = 0
-    eta_dsa: float = 0
+    eta_ae: float = 1.0
+    eta_lsa: float = 0.0
+    eta_msa: float = 0.0
+    eta_dsa: float = 0.0
 
     # assessment of abstraction
     nsteps_pred_latent: int = 100
@@ -37,4 +38,17 @@ class Config(NamedTuple):
 
 
 if __name__ == "__main__":
+    cfg = Config()
+    parser = argparse.ArgumentParser()
+    for field in cfg._fields:
+        if isinstance(cfg.__getattribute__(field), bool):
+            parser.add_argument(f"--{field}", type=eval)
+        else:
+            parser.add_argument(
+                f"--{field}",
+                type=type(cfg.__getattribute__(field)),
+                default=cfg.__getattribute__(field),
+            )
+    args = parser.parse_args()
+    print("Running with args", args)
     run(Experiment, Config())
