81a0da944e1407ea0391ffa40e94885397f410a5
Julius Taylor <juliustaylor@protonmail.com>
2021-04-12 12:46:33 +0200

    Fix typo

--- run_single_slurm.py
+++ run_single_slurm.py
@@ -10,11 +10,9 @@ parser.add_argument("--path", type=str, required=True)
 parser.add_argument("--rank", type=int, required=True)
 merge_cfg_with_cli(cfg, parser)
 args = parser.parse_args()
-
+if not os.path.exists(args.path):
+    os.makedirs(args.path)
 writer = MultiProcessingWriter(args.path, rank=args.rank)
 reader = TidyReader(args.path)
-print(os.getcwd())
-print(args)
-print(vars(args))
 exp = Experiment(cfg, args.rank, writer, reader, args.path)
 exp.run(args)
