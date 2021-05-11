6989392a952732ab4ea855422d6802d1e90b124a
Julius Taylor <juliustaylor@protonmail.com>
2021-05-11 13:47:31 +0200

    Add config

--- compute_cross_cls.py
+++ compute_cross_cls.py
@@ -1,16 +1,20 @@
-from experiments.experiment import BaseConfig
-from chunked_writer import TidyWriter
+import os
 import pathlib
 from typing import NamedTuple
+
+from experiments.experiment import BaseConfig
 from experiments.plot_cross_agent_cls import Experiment
 from slurm_runner_cross_agent import run_single_from_sweep_slurm
 
-
 PATH = "results/jeanzay/results/sweeps/shared_ref_mnist/2021-04-20/14-58-18"
+root_path = os.path.join(os.path.expandvars("$SCRATCH"), PATH)
 
 
 class Config(BaseConfig):
-    pass
+    path: str
+    nsteps: int
+    bsize: int
+    eval_bsize: int
 
 
 class RunnerCfg(NamedTuple):
@@ -25,10 +29,12 @@ class RunnerCfg(NamedTuple):
 
 
 exp_paths = pathlib.Path(PATH).glob("*")
-cfg = Config(nogpu=False, nprocs=5)
 runner_cfg = RunnerCfg()
 
 for path in exp_paths:
+    cfg = Config(
+        nogpu=False, nprocs=5, path=path, nsteps=2500, bsize=2048, eval_bsize=8192
+    )
     for rank in range(runner_cfg.nprocs):
         run_single_from_sweep_slurm(
             cfg, runner_cfg, path, rank, str(path) + f"-rank_{rank}"
