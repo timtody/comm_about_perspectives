02a9527c465a8f383e2ebc54379bd9ebc7536573
Julius Taylor <juliustaylor@protonmail.com>
2021-05-11 18:58:31 +0200

    Fix typo

--- run_exp.py
+++ run_exp.py
@@ -3,10 +3,12 @@ import argparse
 from chunked_writer import MultiProcessingWriter
 from compute_cross_cls import Config
 from experiments.plot_cross_agent_cls import Experiment
+from functions import merge_cfg_with_cli
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--path", required=True)
 parser.add_argument("--rank", required=True)
+merge_cfg_with_cli(Config, parser)
 args = parser.parse_args()
 
 cfg = Config()
