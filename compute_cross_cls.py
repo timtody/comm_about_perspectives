11573a742b60cdecc467559fc029ab60c4a30fc0
Julius Taylor <juliustaylor@protonmail.com>
2021-05-11 13:54:18 +0200

    Correct the path

--- compute_cross_cls.py
+++ compute_cross_cls.py
@@ -6,7 +6,7 @@ from experiments.experiment import BaseConfig
 from experiments.plot_cross_agent_cls import Experiment
 from slurm_runner_cross_agent import run_single_from_sweep_slurm
 
-PATH = "results/jeanzay/results/sweeps/shared_ref_mnist/2021-04-20/14-58-18"
+PATH = "results/sweeps/shared_ref_mnist/2021-04-20/14-58-18"
 PATH = os.path.join(os.path.expandvars("$SCRATCH"), PATH)
 
 
