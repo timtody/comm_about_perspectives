78e808bbd00abc491e1a723a2d0b155cad0ae996
Julius Taylor <juliustaylor@protonmail.com>
2021-05-11 19:05:49 +0200

    Fix typo

--- run_exp.py
+++ run_exp.py
@@ -13,7 +13,7 @@ args = parser.parse_args()
 
 cfg = Config()
 writer = MultiProcessingWriter(args.path, rank=args.rank)
-cfg.path = args.path
+setattr(cfg, "path", args.path)
 experiment = Experiment(cfg, args.rank, writer, None, args.path)
 experiment.run(cfg)
 experiment.writer.close()
