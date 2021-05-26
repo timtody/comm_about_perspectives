f89289565a391b29cd89c27a960053f43f2fc760
Julius Taylor <juliustaylor@protonmail.com>
2021-05-26 22:29:44 +0200

    decentralised

--- experiments/robustness.py
+++ experiments/robustness.py
@@ -28,7 +28,7 @@ class Config(NamedTuple):
 
 class Experiment(BaseExperiment):
     def run(self, cfg: Config):
-        centralised = True
+        centralised = False
 
         if centralised:
             base_path = os.path.join(os.path.expandvars("$SCRATCH"),
