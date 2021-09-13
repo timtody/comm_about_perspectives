80f91954bb6acf1d4c166a7ed6106d41d09ab53b
Julius Taylor <juliustaylor@protonmail.com>
2021-09-13 14:22:04 +0200

    Print cuda availability

--- compute_loss_data_curve.py
+++ compute_loss_data_curve.py
@@ -11,6 +11,29 @@ from cifar import CifarDataset
 from torch import Tensor
 from autoencoder import _AutoEncoder
 from functools import reduce
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def plot_curves(results, ns, path):
+    sns.set_palette(sns.color_palette("Set1"))
+    ax = sns.lineplot(
+        data=results,
+        x="Size",
+        y="Value",
+        # hue="Metric",
+        legend="brief",
+        # style="name",
+        markers=True,
+        dashes=False,
+    )
+    # handles, labels = ax.get_legend_handles_labels()
+    # ax.legend(handles=handles[1:], labels=labels[1:])
+    plt.yscale("log")
+    plt.xscale("log")
+    plt.ylabel("Validation loss")
+    plt.xlabel("Dataset size")
+    plt.savefig(f"{path}_reprieve_curves.pdf")
 
 
 class CifarAutoEncoder(_AutoEncoder, nn.Module):
@@ -80,11 +103,11 @@ def split(x: np.ndarray, train_size=0.8):
 
 
 def compute_curve(
-    X: np.ndarray, y: np.ndarray, ae: CifarAutoEncoder, sizes: list, rank: int
+    X: np.ndarray, y: np.ndarray, ae: CifarAutoEncoder, sizes: np.ndarray, rank: int
 ) -> pd.DataFrame:
     data = []
     for size in sizes:
-        indices = np.random.randint(len(y), size=size)
+        indices = np.random.randint(len(y), size=int(size))
         X_sub, y_sub = X[indices], y[indices]
         X_train, X_test = split(X_sub)
         y_train, y_test = split(y_sub)
@@ -93,8 +116,8 @@ def compute_curve(
         opt = optim.Adam(mlp.parameters())
         for _ in range(10):
             mlp = train_fn(mlp, (X_train, y_train), opt, repr_fn=ae.encode)
-        loss, acc = eval_fn(mlp, (X_test, y_test), repr_fn=ae.encode)
-        print(loss, acc)
+            loss, acc = eval_fn(mlp, (X_test, y_test), repr_fn=ae.encode)
+            print(loss, acc)
         data.append((rank, size, "Loss", loss))
         data.append((rank, size, "Accuracy", acc))
     df = pd.DataFrame(data, columns=["Rank", "Size", "Metric", "Value"])
@@ -118,7 +141,12 @@ def load_encoder(path, rank, use_gpu):
 
 
 def gather_results(
-    X: np.ndarray, y: np.ndarray, sizes: list, seeds: int, weights_path: str, use_gpu: bool
+    X: np.ndarray,
+    y: np.ndarray,
+    sizes: np.ndarray,
+    seeds: int,
+    weights_path: str,
+    use_gpu: bool,
 ) -> pd.DataFrame:
     results = [
         compute_curve(X, y, load_encoder(weights_path, rank, use_gpu), sizes, rank)
@@ -128,15 +156,23 @@ def gather_results(
 
 
 def main(args: argparse.Namespace):
-    dataset = CifarDataset(f"CIFAR{args.n_classes}")
-    X, y = dataset.eval.data.transpose([0, 3, 1, 2]) / 255.0, dataset.eval.targets
-    results = gather_results(X, y, args.sizes, args.seeds, args.weights_path, args.use_gpu)
-    results.to_csv("results/cifar_curves.csv")
+    print("Cuda available:", torch.cuda.is_available())
+    sizes = np.logspace(np.log10(args.min_size), np.log10(args.max_size), num=args.steps)
+    if not args.only_plot:
+        dataset = CifarDataset(f"CIFAR{args.n_classes}")
+        X, y = dataset.eval.data.transpose([0, 3, 1, 2]) / 255.0, dataset.eval.targets
+
+        results = gather_results(X, y, sizes, args.seeds, args.weights_path, args.use_gpu)
+        results.to_csv("results/cifar_curves.csv")
+    plot_curves(pd.read_csv("results/cifar_curves.csv"), sizes, "results/testtest")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--sizes", type=eval, default="[10, 100, 1000, 10000]")
+    parser.add_argument("--min_size", type=int, default=10)
+    parser.add_argument("--max_size", type=int, default=10000)
+    parser.add_argument("--steps", type=int, default=10)
+    # parser.add_argument("--sizes", type=eval, default="[10, 100, 1000, 10000]")
     parser.add_argument("--interpolation_steps", type=int, default=10)
     parser.add_argument("--seeds", type=int, default=5)
     parser.add_argument("--n_classes", type=int, default=10, choices=(10, 100))
@@ -145,4 +181,5 @@ if __name__ == "__main__":
         "--metric", type=str, default="accuracy", choices=("accuracy", "loss")
     )
     parser.add_argument("--weights_path", type=str, required=True)
+    parser.add_argument("--only_plot", action="store_true", dest="only_plot")
     main(parser.parse_args())
